b77792176656dd13bc337ed62981187135baca6a
Zhaoning Wang <t-zhwang@microsoft.com>
2019-07-31 10:49:05 -0700

    #2 commit of not building test

--- RdlMigration/ConvertRDL.cs
+++ RdlMigration/ConvertRDL.cs
@@ -15,9 +15,9 @@ using Microsoft.PowerBI.Api.V2.Models;
 using Microsoft.Rest;
 using Newtonsoft.Json.Linq;
 using RdlMigration.ReportServerApi;
-using static RdlMigration.ElementNameConstants
+using static RdlMigration.ElementNameConstants;
 
-namespace RdlMigration
+namespace RdlMigrationn
 {
     /// <summary>
     /// The class that actually does the conversion.
