b982fbca605518fa17bc88fa085b0c86b7a688f6
Guilherme Boreki <guboreki@microsoft.com>
2019-08-06 13:30:51 -0700

    Add netstandard

--- RdlMigration/PowerBIClientWrapper.cs
+++ RdlMigration/PowerBIClientWrapper.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Microsoft.PowerBI.Api.V2;
 using Microsoft.PowerBI.Api.V2.Models;
